19913881496347710fbb8eae540c483ff303196b
Vianey González Guzmán <78153997+Vianey25@users.noreply.github.com>
2023-12-10 22:25:49 -0600

    Cambios

--- pwa.js
+++ pwa.js
@@ -37,13 +37,56 @@ imageContainer.addEventListener('mouseout', () => {
 });
 
 // Añadimos eventos clic a los botones de los edificios
-buildingButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        // Obtén el texto del botón y muestra la información correspondiente
-        const buildingName = button.textContent;
-        buildingInfo.textContent = `Información sobre ${buildingName}. Aquí puedes mostrar detalles sobre el edificio.`;
+    buildingButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            // Obtén el texto del botón y la información asociada
+            const buildingName = button.textContent;
+            const buildingInfoText = button.getAttribute('data-info');
+            const buildingImage = button.getAttribute('data-image');
+
+            // Muestra la información correspondiente
+            buildingInfo.textContent = `${buildingInfoText}`;
+            const image360 = document.getElementById('360-image');
+            image360.src = buildingImage;
     });
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    const sceneContainer = document.getElementById('360-scene');
+
+    if (sceneContainer) {
+        const button360View = document.getElementById('360-view-button');
+        const buildingButtons = document.querySelectorAll('.building-button');
+
+        button360View.addEventListener('click', function () {
+            // Crea la escena en 360 grados dinámicamente al hacer clic en el botón
+            const equirectangularImage = './A.jpg';
+
+            // Crea la escena A-Frame y la imagen en 360 grados
+            const sceneElement = document.createElement('a-scene');
+            const skyElement = document.createElement('a-sky');
+            skyElement.setAttribute('src', equirectangularImage);
+            skyElement.setAttribute('rotation', '0 -90 0');
+
+            // Limpia el contenedor antes de agregar la nueva escena
+            while (sceneContainer.firstChild) {
+                sceneContainer.removeChild(sceneContainer.firstChild);
+            }
+
+            // Agrega la escena y la imagen al contenedor
+            sceneElement.appendChild(skyElement);
+            sceneContainer.appendChild(sceneElement);
+
+            // Muestra los botones de edificio al hacer clic en el botón "360 grados"
+            buildingButtons.forEach(button => {
+                button.style.display = 'block';
+            });
+        });
+    } else {
+        console.error('No se encontró el contenedor de escena (sceneContainer).');
+    }
+});
+
 
 // guardar datos en consola 
 const browserInformation = {
